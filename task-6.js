7c87dc29d671908c2c77663eeb448d0b95df84e4
Omelchenkormn <omelchenkormn@gmail.com>
2022-04-05 21:13:37 +0300

    Dorab

--- task-6.js
+++ task-6.js
@@ -1,45 +1,29 @@
-// let input;
-// let total = 0;
-// const numbers = [];
- 
-// // numbers.push(input);
-// // input = Number(prompt('Введите число'));
-
-// // input.push(total);
-
-
-// while (true) {
-//     input = prompt('Введите число');
-//     if (isNaN(Number(input))) {
-//         alert('Попробуйте еще раз, введите число')
-//         continue;
-//     }
-//     if (input === null) {
-//         break;
-//     }
-//     input = Number(input);
-//     numbers.push(input);
-// }
-
-// console.log(`Сумма ${total}`);
-
-
 let input;
 let total = 0;
 const array = [];
 while (true) {
-input = prompt('Вводим аккуратно число');
-if (isNaN(Number(input))) {
-    alert('я же сказал,аккуратно,это не число');
+    input = prompt('Введите число');
+    
+if (isNaN(input)) {
+    alert('ошибка, это не число');
     continue;
-}
+    }
+    
 if (input === null) {
     break;
-}
+    }
+
+
 input = Number(input);
 array.push(input)
-total+=input;
+
+
 }
-console.log(`вот твоя  сумма ${total}`);
+for (let i = 0; i < array.length; i+=1) {
+        total += array[i];
+        
+}
+
+console.log(`Общая сумма чисел равна ${total}`);
 console.log(array);
 
